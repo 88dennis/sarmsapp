5385f1f38e4b2271ffee3c0563d75b176d5271cf
Dennis Sarmiento <dennissarmiento8080@gmail.com>
2021-01-09 23:50:39 -0500

    update useHistory

--- src/Page2.js
+++ src/Page2.js
@@ -1,12 +1,24 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link } from 'react-router-dom'
+import { useHistory } from "react-router-dom";
+
 
 const Page1 = () => {
+
+const history = useHistory();
+
+  const routeChange = () =>{ 
+    let path = `/`; 
+    history.replace(path);
+    console.log(history)
+    }
     return (
         <div>
             page2
 
+        <button onClick={()=>routeChange()}> TO PAGE1</button>
             <Link to='/'>TO PAGE 1</Link>
+
 
         </div>
     )
